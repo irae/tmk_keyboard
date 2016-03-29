8cbba24fc30da339ad7ef8e8f64da793a4f75e61
Irae <irae@irae.pro.br>
2016-03-29 01:28:25 -0700

    Fix small app menu bug, now properlly on Fn+Z

--- keyboard/s60-x/keymap_mac_pok3r.c
+++ keyboard/s60-x/keymap_mac_pok3r.c
@@ -40,7 +40,7 @@ const uint8_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      GRV,   F1,   F2,   F3,   F4,   F5,   F6,   F7,   F8,   F9,  F10,  F11,  F12,   NO,  DEL, \
     TRNS, MPRV, MPLY, MNXT, TRNS, TRNS, TRNS, PGUP,   UP, PGDN, PSCR, SLCK, PAUS, TRNS,       \
     TRNS, TRNS, VOLD, VOLU, MUTE, TRNS, HOME, LEFT, DOWN, RGHT,  INS,  DEL,   NO, TRNS,       \
-    TRNS,  APP, TRNS, TRNS, TRNS, TRNS, TRNS,  END, TRNS, TRNS, TRNS, TRNS,   NO, CAPS,   NO, \
+    TRNS,   NO,  APP, TRNS, TRNS, TRNS, TRNS,  END, TRNS, TRNS, TRNS, TRNS,   NO, CAPS,   NO, \
     TRNS, TRNS, TRNS,                   TRNS,             TRNS, TRNS, EJCT, TRNS),
 };
 
